4e6e3098e6e4b6ad8a4449f8a5c729df76578a19
Belikov <l.belikov@pushapp.ru>
2020-08-14 07:24:09 +0300

    Подключить vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import store from './store'
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  store,
+})
